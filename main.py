6a2f07efda63bc58bdcea014c464723634fbeade
cesar <cesardelaygues@hotmail.fr>
2023-09-20 08:19:05 +0200

    commit final

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from flask import Flask
 
 app = Flask(__name__)
-
 @app.route("/")
-def root():
-    return "Hello from Space! 🚀"
+def hello_world():
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-GKPDQR0FNY"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-GKPDQR0FNY');
+    </script>
+    """
+    return prefix_google + "Hello World"
+
+
